38158433db3259689ac2cebbd987be0b2d5b6eaf
Sivvio <31251239+Sivvio@users.noreply.github.com>
2022-01-24 19:49:52 +0000

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -8,9 +8,5 @@ export default () => ({
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         entities: [__dirname + '/**/*.entity{.ts,.js}']
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRATION
     }
 });
