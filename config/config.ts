1db9405844f7f37daf6a454724f2cbe312358f55
silvio_piccolo_aps2 <sivvio.piccolo@gmail.com>
2022-01-26 21:41:09 +0000

    fixed entities typeorm path

--- config/config.ts
+++ config/config.ts
@@ -7,7 +7,7 @@ export default () => ({
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        entities: [__dirname + '/**/*.entity{.ts,.js}']
+        entities: ['dist/**/*.entity{.ts,.js}'],
     },
     jwt: {
         secret: process.env.JWT_SECRET,
